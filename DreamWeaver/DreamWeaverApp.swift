9fbaddb065e16a8aabe484756503f4122772be42
huyuchao <huyuchao382@gmail.com>
2026-07-26 23:56:01 +0800

    Initial Commit

---
+++ DreamWeaver/DreamWeaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWeaverApp.swift
+//  DreamWeaver
+//
+//  Created by hu on 2026/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamWeaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
